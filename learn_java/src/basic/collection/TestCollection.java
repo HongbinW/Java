56886c3831189a72e3272c402530d1d13b857920
HongbinW <501245176@qq.com>
2018-12-26 23:07:58 +0800

    Collection1

---
+++ learn_java/src/basic/collection/TestCollection.java
@@ -0,0 +1,14 @@
+package basic.collection;
+/*
+    1.存储对象可以考虑：①数组 ②集合
+    2.数组存储对象的特点：Student[] stu = new Student[20]; stu[0] = new Student();……
+        >弊端：①一旦创建，其长度不可变。②实际存储量不可直接知
+    3.集合
+        -----Collection接口
+                ----Set：元素无序，不可重复的集合
+                ----List:元素有序，可重复的集合（“动态”数组）
+        -----Map接口：具有映射关系“key-value对”的集合
+ */
+public class TestCollection {
+
+}
--- learn_java/src/basic/exception/TestShoudongpao.java
+++ learn_java/src/basic/exception/TestShoudongpao.java
@@ -6,8 +6,8 @@ public class TestShoudongpao {
     public static void main(String[] args) {
         Circle c1 = new Circle(2.3);
         Circle c2 = new Circle(2.3);
-        System.out.println(c1.compareTo(c2));
         System.out.println(c1.compareTo(new String("AA")));
+        System.out.println(c1.compareTo(c2));
     }
 }
 class Circle{
