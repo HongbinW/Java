b0f4be33acdb73cd9a285a61d07df3454ab81413
HongbinW <501245176@qq.com>
2019-01-16 21:27:42 +0800

    如何按照value排列TreeMap

---
+++ learn_java/src/questions/TestTreeMap.java
@@ -0,0 +1,9 @@
+package questions;
+//TreeMap如何根据value排序呢？
+/*
+    思路一：把TreeMap的EntrySet转换成list，然后使用Collections.sor排序
+    思路二：将原TreeMap集合的键值对互换,存入新的TreeMap集合
+    思路三：传入Key，然后key.get()来比较
+ */
+public class TestTreeMap {
+}
