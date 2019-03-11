35bbb21af4d6de3d532bd5ed739ebdfa1a9875d6
HongbinW <501245176@qq.com>
2019-03-11 10:30:42 +0800

    BST

--- DataStructure/src/Tree/BST.java
+++ DataStructure/src/Tree/BST.java
@@ -1,4 +1,11 @@
 package Tree;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 //二分搜索树
 public class BST <E extends Comparable<E>> {
     private class Node{
@@ -93,6 +100,7 @@ public class BST <E extends Comparable<E>> {
             return contains(node.right,e);
         }
     }
+
     //二分搜索树的前序遍历
     public void preOrder(){
         preOrder(root);
@@ -104,6 +112,197 @@ public class BST <E extends Comparable<E>> {
         System.out.println(node.e);
         preOrder(node.left);
         preOrder(node.right);
+    }
+
+    //非递归
+    public void preOrderNR(){
+        Stack<Node> stack = new Stack<>();
+        if(root != null) {
+            stack.push(root);
+        }
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            System.out.println(node.e);
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+        }
+    }
+
+    //二分搜索树的中序遍历    :结果为从小到大排序
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null)
+            return;
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    //二分搜索树的后序遍历
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    //层序遍历
+    public void levelOrder(){
+        Queue<Node> queue = new LinkedList<>();     //Queue本身是个接口，选择链表方式实现
+        queue.add(root);
+        while(!queue.isEmpty()){
+            Node node = queue.remove();
+            System.out.println(node.e);
+            if(node.left != null){
+                queue.add(node.left);
+            }
+            if(node.right != null){
+                queue.add(node.right);
+            }
+        }
+    }
+
+    //返回二分搜索树的最小元素
+    public E minimum(){
+       return minimum(root) == null ? null : minimum(root).e;
+    }
+    private Node minimum(Node node){
+        if(node == null)
+            return null;
+        if(node.left == null){
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    //返回二分搜索树的最大元素
+    public E maximum(){
+        return maximum(root) == null ? null : maximum(root).e;
+    }
+    private Node maximum(Node node){
+        if(root == null)
+            return null;
+        if(node.right == null){
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    //删除二分搜索树的最小节点
+    public E removeMin(){
+        E res = minimum();
+        root = removeMin(root);
+        return res;
+    }
+    //删除以node为根的二分搜索树中的最小节点
+    //返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node){
+        if(node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size --;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;        //若是最小节点，则返回rightNode,否则，还是返回原来的node，保持原来结构不变
+    }
+
+    //删除二分搜索树的最大节点
+    public E removeMax(){
+        E res = maximum();
+        root = removeMax(root);
+        return res;
+    }
+    private Node removeMax(Node node){
+        if(node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size --;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    //删除二分搜索树中任意一个节点
+    //若该节点既有左子树又有右子树，则选择右子树中最小的（最左的）那个节点来替换该节点,当然也可以用左子树中最大的
+    public void remove(E e){
+        root = remove(root,e);
+    }
+    //删除以node为根的二分搜索树中的值为e的节点，递归算法
+    //返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, E e){
+        if(node == null){
+            return null;
+        }
+        if(e.compareTo(node.e) == 0){
+            Node newNode = minimum(node.right);
+            if(newNode == null){
+                newNode = node.left;
+                size --;
+                node.left = null;
+                return newNode;
+            }
+            newNode.right = removeMin(node.right);
+            newNode.left = node.left;
+            node = null;
+            return newNode;
+        }
+        else if(e.compareTo(node.e) < 0){
+            node.left = remove(node.left,e);
+        }else{
+            node.right = remove(node.right,e);
+        }
+        return node;
+    }
+
+    /**
+     * 在二分搜索树中找出比指定元素e大的最小值
+     */
+    public E ceil(E e){
+
+        return null;
+    }
+    private Node ceil(Node node,E e){
+        if(node == null){
+
+        }
+        if(e.compareTo(node.e) < 0){
+            node.left = ceil(node.left,e);
+        }else if(e.compareTo(node.e) > 0){
+            node.right = ceil(node.right,e);
+        }
+        return null;
+    }
+
+    /**
+     * 在二分搜索树中找出比指定元素e小的最大值
+     */
+
+
+    /**
+     * 在二分搜索树中查询指定元素排名第几
+     */
+    public int rank(E e){
+        return 0;
+    }
+
+    /**
+     * 在二分搜索树中查询排名第index的元素是谁
+     * note:对于rank和select，可以在node节点中多加一个size变量，用来指示以该节点为根节点的树中 吧有多少个节点
+     */
+    public E select(int index){
+        return null;
     }
 
     //基于前序遍历的toString方法
--- DataStructure/src/Tree/BSTMain.java
+++ DataStructure/src/Tree/BSTMain.java
@@ -1,14 +1,16 @@
 package Tree;
 
+import java.util.ArrayList;
+
 public class BSTMain {
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int[] nums = {5,3,6,2,4,8};
+        int[] nums = {5,3,9,1,4,7,14,6,8};
         for(int i = 0; i < nums.length; i++){
             bst.add(nums[i]);
         }
-        bst.preOrder();
-        System.out.println();
+        bst.remove(8);
+//        bst.removeMin();
         System.out.println(bst);
     }
 }
