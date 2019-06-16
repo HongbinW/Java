299a8975ca6af8765301584b6c141db503976b65
HongbinW <501245176@qq.com>
2019-06-16 10:32:35 -0400

    JZOFFER

--- Algorithm/src/JZOffer/J_knowledgeTransfer/M58_Q1_ReverseSentence.java
+++ Algorithm/src/JZOffer/J_knowledgeTransfer/M58_Q1_ReverseSentence.java
@@ -1,4 +1,17 @@
 package JZOffer.J_knowledgeTransfer;
 
 public class M58_Q1_ReverseSentence {
+    public String ReverseSentence(String str) {
+        if(str == null || str.trim().length() == 0)
+            return str;
+        String[] strs = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strs.length-1; i >= 0; i --){
+            sb.append(strs[i]);
+            if (i != 0){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
 }
