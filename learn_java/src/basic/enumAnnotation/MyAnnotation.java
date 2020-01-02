975e5fcde3ef28e8cdaa0fa1f66dd0b6f339023d
HongbinW <501245176@qq.com>
2020-01-02 14:59:45 +0800

    annotation

--- learn_java/src/basic/enumAnnotation/MyAnnotation.java
+++ learn_java/src/basic/enumAnnotation/MyAnnotation.java
@@ -1,4 +1,10 @@
 package basic.enumAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 //自定义注解
 public @interface MyAnnotation {
     String value() default "hello";
